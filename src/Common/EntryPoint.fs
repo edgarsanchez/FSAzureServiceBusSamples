22426951fe5420e35c508435d5f38ac54ffb3798
edgarsanchez <edgar.yachakuq@hotmail.com>
2020-09-18 15:19:16 -0500

    DuplicateDetection implemented

---
+++ src/Common/EntryPoint.fs
@@ -0,0 +1,49 @@
+﻿module EntryPoint
+    open System
+    open System.IO
+
+    // This is a common entry point class for all samples. It loads the properties
+    // stored in the "azure-msg-samples.properties" file from the user profile
+    // and then allows override of the settings from environment variables.
+
+    let basicQueueName = "BasicQueue"
+    let partitionedQueueName = "PartitionedQueue"
+    let dupdetectQueueName = "DupdetectQueue"
+    let basicTopicName = "BasicTopic"
+    let sessionQueueName = "SessionQueue"
+    let basicQueue2Name = "BasicQueue2"
+    let connectionStringName = "SB_SAMPLES_CONNECTIONSTRING"
+    let samplePropertiesFileName = "azure-msg-config.properties"
+
+    let rec findConnectionString (fs: StreamReader) =
+        if fs.EndOfStream then
+            None
+        else
+            match fs.ReadLine () with
+            | null ->
+                findConnectionString fs
+            | readLine ->
+                let lineWithoutComment = Array.head (readLine.Split '#')
+                let keyValueParts = lineWithoutComment.Split '='
+                if Array.length keyValueParts <= 1 then
+                    findConnectionString fs
+                elif (Array.head keyValueParts).Trim () = connectionStringName then
+                    Some (keyValueParts.[1].Trim ())
+                else
+                    findConnectionString fs
+
+    let runSample _ run =
+        let connectionString =
+            match Environment.GetEnvironmentVariable connectionStringName with
+            | null ->
+                // If there is no environment variable for the connection string, try to get it from a .properties file
+                let settingsFile = Path.Combine (Environment.GetFolderPath Environment.SpecialFolder.UserProfile, samplePropertiesFileName)
+                if File.Exists settingsFile then
+                    use fs = new StreamReader (settingsFile)
+                    findConnectionString fs
+                else
+                    None
+            | envVarValue ->
+                Some envVarValue
+
+        run connectionString |> Async.RunSynchronously
---
+++ src/DuplicateDetection/Program.fs
@@ -0,0 +1,75 @@
+﻿open System
+open Microsoft.Azure.ServiceBus
+open Microsoft.Azure.ServiceBus.Core
+
+let send (connectionString: string) =
+    // Create a sender over the previously configured duplicate-detection
+    // enabled queue.
+    let sender = MessageSender (connectionString, EntryPoint.dupdetectQueueName)
+
+    // Create the message-id 
+    let messageId = string (Guid.NewGuid ())
+    
+    printfn "\tSending messages to %s ..." sender.Path
+    async {
+        // send the first message using the message-id
+        let message = Message (MessageId = messageId, TimeToLive = TimeSpan.FromMinutes 1.)
+        do! sender.SendAsync message |> Async.AwaitTask
+        printfn "\t=> Sent a message with messageId %s" message.MessageId
+
+        // send the second message using the message-id
+        let message2 = Message (MessageId = messageId, TimeToLive = TimeSpan.FromMinutes 1.)
+        do! sender.SendAsync message2 |> Async.AwaitTask
+        printfn "\t=> Sent a duplicate message with messageId %s" message.MessageId
+        
+        do! sender.CloseAsync () |> Async.AwaitTask
+    }
+
+let receive (connectionString: string) =
+    // create receiver over the duplicate-detection enabled queue
+    let receiver = MessageReceiver (connectionString, EntryPoint.dupdetectQueueName, ReceiveMode.PeekLock)
+
+    let rec receiveMessages (previousMessageId: string) =
+        async {
+            // receive a message
+            let! receivedMessage = receiver.ReceiveAsync (TimeSpan.FromSeconds 5.) |> Async.AwaitTask
+            if not (isNull receivedMessage) then
+                // complete the received message
+                printfn "\t<= Received a message with messageId %s" receivedMessage.MessageId
+                do! receiver.CompleteAsync receivedMessage.SystemProperties.LockToken |> Async.AwaitTask
+
+                if previousMessageId.Equals(receivedMessage.MessageId, StringComparison.OrdinalIgnoreCase) then
+                    raise (Exception ("Received a duplicate messsage"))
+                
+                do! receiveMessages receivedMessage.MessageId
+        }
+
+    printfn "\n\tWaiting up to 5 seconds for messages from %s ..." receiver.Path
+    async {
+        do! receiveMessages ""
+        do! receiver.CloseAsync () |> Async.AwaitTask
+
+        printfn "\tDone receiving messages from %s" receiver.Path
+    }
+
+let run connectionString =
+    async {
+        match connectionString with
+        | None ->
+            printfn "No ServiceBus connection string provided"
+        | Some connectionString ->
+            // first send the two messages
+            do! send connectionString
+            // then retrieve the messages
+            do! receive connectionString
+    }
+         
+[< EntryPoint >]
+let main argv =
+    try
+        EntryPoint.runSample argv run
+        0
+    with
+    | exn ->
+        printfn "%O" exn
+        1
